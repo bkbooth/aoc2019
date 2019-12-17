d0946d0284b7c2476b2cc6310f24ee5683fbf8c6
Ben Booth <bkbooth@gmail.com>
2019-12-17 23:44:13 +1100

    Complete day 5 part 2

--- day5/script.js
+++ day5/script.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { intcodeComputer } = require('./intcodeComputer');
 
 const INPUT_FILE = path.join(__dirname, 'input.txt');
-const USER_INPUT = 1;
+const USER_INPUT = 5;
 
 fs.readFile(INPUT_FILE, 'utf8')
   .then(input => input.split(','))
