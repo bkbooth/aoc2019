6880eba917673476d7ff95846bd40fdb8b3663db
Ben Booth <bkbooth@gmail.com>
2019-12-17 18:15:48 +1100

    Get day 5 specs passing
    Improvements to intcode computer

--- day5/intcodeComputer.js
+++ day5/intcodeComputer.js
@@ -1,22 +1,69 @@
-function intcodeComputer(memory) {
+function intcodeComputer(initialMemory, ...inputs) {
+  let memory = [...initialMemory];
   let instructionPointer = 0;
-  let opcode = memory[instructionPointer];
-  let parameter1Address, parameter2Address, destinationAddress;
-  let parameter1, parameter2;
+  let inputPointer = 0;
+
+  let { opcode, params, paramModes } = parseInstruction(memory[instructionPointer]);
   while (opcode !== 99) {
-    parameter1Address = memory[instructionPointer + 1];
-    parameter2Address = memory[instructionPointer + 2];
-    destinationAddress = memory[instructionPointer + 3];
+    if (opcode === 1) {
+      // add
+      const param1 = getParam(memory, memory[instructionPointer + 1], paramModes[0]);
+      const param2 = getParam(memory, memory[instructionPointer + 2], paramModes[1]);
+      const destinationAddress = memory[instructionPointer + 3];
+      memory[destinationAddress] = param1 + param2;
+    } else if (opcode === 2) {
+      // multiply
+      const param1 = getParam(memory, memory[instructionPointer + 1], paramModes[0]);
+      const param2 = getParam(memory, memory[instructionPointer + 2], paramModes[1]);
+      const destinationAddress = memory[instructionPointer + 3];
+      memory[destinationAddress] = param1 * param2;
+    } else if (opcode === 3) {
+      // input
+      const input = inputs[inputPointer++];
+      const destinationAddress = memory[instructionPointer + 1];
+      memory[destinationAddress] = input;
+    } else if (opcode === 4) {
+      // output
+      const valueAddress = memory[instructionPointer + 1];
+      const value = paramModes[0] === 1 ? valueAddress : memory[valueAddress];
+      console.log('Diagnostic code:', value);
+    } else {
+      throw new Error(`Unknown upcode ${opcode}`);
+    }
 
-    parameter1 = memory[parameter1Address];
-    parameter2 = memory[parameter2Address];
-    memory[destinationAddress] = opcode === 1 ? parameter1 + parameter2 : parameter1 * parameter2;
-
-    instructionPointer += 4;
-    opcode = memory[instructionPointer];
+    instructionPointer += params + 1;
+    ({ opcode, params, paramModes } = parseInstruction(memory[instructionPointer]));
   }
 
   return memory;
 }
 
-module.exports = { intcodeComputer };
+const OPCODE_PARAMS = {
+  99: 0,
+  1: 3,
+  2: 3,
+  3: 1,
+  4: 1,
+};
+
+function parseInstruction(instruction) {
+  let padded = String(instruction).padStart(5, '0');
+
+  let opcode = Number(padded.slice(-2));
+  let params = OPCODE_PARAMS[opcode];
+  let paramModes = Array(params)
+    .fill()
+    .map((_, index) => {
+      const start = -2 - (index + 1);
+      const end = start + 1;
+      return Number(padded.slice(start, end));
+    });
+
+  return { opcode, params, paramModes };
+}
+
+function getParam(memory, address, paramMode) {
+  return paramMode === 1 ? address : memory[address];
+}
+
+module.exports = { intcodeComputer, parseInstruction };
--- day5/intcodeComputer.spec.js
+++ day5/intcodeComputer.spec.js
@@ -1,7 +1,66 @@
 const { runTestCases } = require('../utils/runTestCases');
-const { intcodeComputer } = require('./intcodeComputer');
+const { intcodeComputer, parseInstruction } = require('./intcodeComputer');
 
-runTestCases('intcodeComputer', intcodeComputer, [
+runTestCases('parseInstruction', parseInstruction, [
+  {
+    input: 1,
+    expectedOutput: {
+      opcode: 1,
+      params: 3,
+      paramModes: [0, 0, 0],
+    },
+  },
+  {
+    input: 2,
+    expectedOutput: {
+      opcode: 2,
+      params: 3,
+      paramModes: [0, 0, 0],
+    },
+  },
+  {
+    input: 99,
+    expectedOutput: {
+      opcode: 99,
+      params: 0,
+      paramModes: [],
+    },
+  },
+  {
+    input: 1002,
+    expectedOutput: {
+      opcode: 2,
+      params: 3,
+      paramModes: [0, 1, 0],
+    },
+  },
+  {
+    input: 1101,
+    expectedOutput: {
+      opcode: 1,
+      params: 3,
+      paramModes: [1, 1, 0],
+    },
+  },
+  {
+    input: 3,
+    expectedOutput: {
+      opcode: 3,
+      params: 1,
+      paramModes: [0],
+    },
+  },
+  {
+    input: 104,
+    expectedOutput: {
+      opcode: 4,
+      params: 1,
+      paramModes: [1],
+    },
+  },
+]);
+
+runTestCases('intcodeComputer', input => intcodeComputer(input, 1, 2, 3), [
   { input: [1, 0, 0, 0, 99], expectedOutput: [2, 0, 0, 0, 99] },
   { input: [2, 3, 0, 3, 99], expectedOutput: [2, 3, 0, 6, 99] },
   { input: [2, 4, 4, 5, 99, 0], expectedOutput: [2, 4, 4, 5, 99, 9801] },
@@ -12,5 +71,21 @@ runTestCases('intcodeComputer', intcodeComputer, [
   {
     input: [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
     expectedOutput: [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+  },
+  {
+    input: [1002, 4, 3, 4, 33],
+    expectedOutput: [1002, 4, 3, 4, 99],
+  },
+  {
+    input: [1101, 100, -1, 4, 0],
+    expectedOutput: [1101, 100, -1, 4, 99],
+  },
+  {
+    input: [3, 0, 99],
+    expectedOutput: [1, 0, 99],
+  },
+  {
+    input: [104, 56, 99],
+    expectedOutput: [104, 56, 99],
   },
 ]);
