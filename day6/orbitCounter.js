86c46a1247307ce9343b612038cab9a0109ffeaf
Ben Booth <bkbooth@gmail.com>
2019-12-18 10:25:24 +1100

    Get day6 spec passing

--- day6/orbitCounter.js
+++ day6/orbitCounter.js
@@ -1,5 +1,12 @@
-function orbitCounter(map) {
-  return 1;
+function orbitCounter(inputMap) {
+  const map = inputMap.map(node => node.split(')'));
+  return map.reduce((orbits, [orbitee]) => orbits + countIndirectOrbits(map, orbitee, 1), 0);
+}
+
+function countIndirectOrbits(map, searchObject, orbits) {
+  const orbit = map.find(([, orbiter]) => orbiter === searchObject);
+  if (orbit) return countIndirectOrbits(map, orbit[0], orbits + 1);
+  else return orbits;
 }
 
 module.exports = { orbitCounter };
