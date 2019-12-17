66caf21b6630755e2bfd833289f6a05d45c32702
Ben Booth <bkbooth@gmail.com>
2019-12-17 13:56:47 +1100

    Complete validatePassword function

--- day4/validatePassword.js
+++ day4/validatePassword.js
@@ -1,5 +1,20 @@
 function validatePassword(password) {
-  return null;
+  // Ensure input is 6 digits only
+  if (!/^[0-9]{6,6}$/.test(password)) return false;
+
+  let hasDouble = false;
+  for (let i = 0; i < password.length - 1; i++) {
+    const digit = password[i];
+    const nextDigit = password[i + 1];
+
+    // Ensure digits never decrease
+    if (nextDigit < digit) return false;
+
+    // Check for double
+    if (digit === nextDigit) hasDouble = true;
+  }
+
+  return hasDouble;
 }
 
 module.exports = { validatePassword };
