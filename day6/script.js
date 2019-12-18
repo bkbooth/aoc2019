ff1d84aede20a3ebb8546a1334f20ccb458c89c5
Ben Booth <bkbooth@gmail.com>
2019-12-18 12:01:28 +1100

    Complete day 6 part 2

--- day6/script.js
+++ day6/script.js
@@ -1,11 +1,16 @@
 const fs = require('fs').promises;
 const path = require('path');
-const { orbitCounter } = require('./orbitCounter');
+const { minOrbitalTransfers, orbitCounter } = require('./orbitCounter');
 
 const INPUT_FILE = path.join(__dirname, 'input.txt');
+const TRANSFER_FROM = 'YOU';
+const TRANSFER_TO = 'SAN';
 
 fs.readFile(INPUT_FILE, 'utf8')
   .then(input => input.split('\n'))
   .then(map => map.filter(node => node !== ''))
-  .then(map => orbitCounter(map))
-  .then(orbits => console.log('Orbit count checksum:', orbits));
+  .then(map => [orbitCounter(map), minOrbitalTransfers(map, TRANSFER_FROM, TRANSFER_TO)])
+  .then(([orbits, minTransfers]) => {
+    console.log('Orbit count checksum:', orbits);
+    console.log('Min orbital transfers:', minTransfers);
+  });
