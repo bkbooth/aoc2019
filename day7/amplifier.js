e2995baf3046d160f7276723a717e24389b0fcc4
Ben Booth <bkbooth@gmail.com>
2019-12-18 13:45:56 +1100

    Get amplifier working

---
+++ day7/amplifier.js
@@ -0,0 +1,15 @@
+const { intcodeComputer } = require('./intcodeComputer');
+
+const INITIAL_AMPLIFIER_INPUT = 0;
+
+function amplifier(program, ...phaseSettings) {
+  const amplifiers = phaseSettings.length;
+  let amplifierInput = INITIAL_AMPLIFIER_INPUT;
+  for (let i = 0; i < amplifiers; i++) {
+    amplifierInput = intcodeComputer(program, phaseSettings[i], amplifierInput);
+  }
+
+  return amplifierInput;
+}
+
+module.exports = { amplifier };
