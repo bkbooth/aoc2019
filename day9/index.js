b67a06e34867c71da57ba073f41389ceeaab1ec8
Ben Booth <bkbooth@gmail.com>
2019-12-19 11:52:33 +1100

    Complete day 9 part 2

--- day9/index.js
+++ day9/index.js
@@ -3,10 +3,11 @@ const path = require('path');
 const { intcodeComputer } = require('./intcodeComputer');
 
 const INPUT_FILE = path.join(__dirname, 'input.txt');
-const PROGRAM_INPUT = 1;
+const PROGRAM_INPUT_TEST_MODE = 1;
+const PROGRAM_INPUT_BOOST_MODE = 2;
 
 fs.readFile(INPUT_FILE, 'utf8')
   .then(input => input.trim().split(','))
   .then(memory => memory.map(Number))
-  .then(memory => intcodeComputer(memory, PROGRAM_INPUT))
+  .then(memory => intcodeComputer(memory, PROGRAM_INPUT_BOOST_MODE))
   .then(output => console.log('OUTPUT:', output));
